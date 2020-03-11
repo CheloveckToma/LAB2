10842a2f6ccf914e66d0e85e4f176b60f87a887e
ev83R <ev83RUS@gmail.com>
2020-03-11 15:40:59 +0300

    refactor

--- src/quadrangle/MainClass.java
+++ src/quadrangle/MainClass.java
@@ -9,22 +9,22 @@ public class MainClass {
         try {
 
             Scanner sc = new Scanner(System.in);
-            double firstPointCoordinateX = sc.nextDouble();
-            double firstPointCoordinateY = sc.nextDouble();
+            double firstPointCoordinateX  = sc.nextDouble();
+            double firstPointCoordinateY  = sc.nextDouble();
 
             double secondPointCoordinateX = sc.nextDouble();
             double secondPointCoordinateY = sc.nextDouble();
 
-            double thirdPointCoordinateY = sc.nextDouble();
-            double thirdPointCoordinateX = sc.nextDouble();
+            double thirdPointCoordinateY  = sc.nextDouble();
+            double thirdPointCoordinateX  = sc.nextDouble();
 
             double fourthPointCoordinateX = sc.nextDouble();
             double fourthPointCoordinateY = sc.nextDouble();
 
-            Vector vectorOne = new Vector(firstPointCoordinateX, firstPointCoordinateY, secondPointCoordinateX, secondPointCoordinateY);
-            Vector vectorTwo = new Vector(secondPointCoordinateX, secondPointCoordinateY, thirdPointCoordinateX, thirdPointCoordinateY);
+            Vector vectorOne     = new Vector(firstPointCoordinateX, firstPointCoordinateY, secondPointCoordinateX, secondPointCoordinateY);
+            Vector vectorTwo       = new Vector(secondPointCoordinateX, secondPointCoordinateY, thirdPointCoordinateX, thirdPointCoordinateY);
             Vector vectorThree = new Vector(thirdPointCoordinateX, thirdPointCoordinateY, fourthPointCoordinateX, firstPointCoordinateY);
-            Vector vectorFour = new Vector(fourthPointCoordinateX, fourthPointCoordinateY, firstPointCoordinateX, firstPointCoordinateY);
+            Vector vectorFour    = new Vector(fourthPointCoordinateX, fourthPointCoordinateY, firstPointCoordinateX, firstPointCoordinateY);
 
             Quadrangle quadrangle = new Quadrangle(vectorOne, vectorTwo, vectorThree, vectorFour);
             quadrangle.definition();
--- src/shadow/Shadow.java
+++ src/shadow/Shadow.java
@@ -5,45 +5,45 @@ import java.util.List;
 
 
 public class Shadow {
-    public double calcShadow(List<Vector> snippets) {
+    public double calcShadow(List<Vector> vectors) {
 
-        for (int i = 0; i < snippets.size(); i++) {
-            for (int j = 0; j < snippets.size(); j++) {
-                if (isMerge(snippets.get(i), snippets.get(j))) {
-                    merge(snippets.get(i), snippets.get(j));
-                    removeSnippet(snippets.get(j), snippets);
+        for (int i = 0; i < vectors.size(); i++) {
+            for (int j = 0; j < vectors.size(); j++) {
+                if (isMerge(vectors.get(i), vectors.get(j))) {
+                    merge(vectors.get(i), vectors.get(j));
+                    removeSnippet(vectors.get(j), vectors);
                 }
 
-                if (isRemoveSnippet(snippets.get(i), snippets.get(j))) {
-                    removeSnippet(snippets.get(j), snippets);
+                if (isRemoveSnippet(vectors.get(i), vectors.get(j))) {
+                    removeSnippet(vectors.get(j), vectors);
                 }
             }
         }
 
         double sizeShadow = 0;
 
-        for (Vector snippet : snippets) {
+        for (Vector snippet : vectors) {
             sizeShadow += snippet.calcLength();
         }
 
         return sizeShadow;
     }
 
-    public boolean isMerge(Vector snippet1, Vector snippet2) {
-        return (snippet1.getPoint1().x < snippet2.getPoint1().x && snippet2.getPoint1().x < snippet1.getPoint2().x) ||
-                (snippet1.getPoint1().x < snippet2.getPoint2().x && snippet2.getPoint2().x < snippet1.getPoint2().x);
+    public boolean isMerge(Vector vector1, Vector vector2) {
+        return (vector1.getPoint1().x < vector2.getPoint1().x && vector2.getPoint1().x < vector1.getPoint2().x) ||
+                (vector1.getPoint1().x < vector2.getPoint2().x && vector2.getPoint2().x < vector1.getPoint2().x);
     }
 
-    public boolean isRemoveSnippet(Vector snippet1, Vector snippet2) {
-        return (snippet1.getPoint1().x < snippet2.getPoint1().x && snippet2.getPoint1().x < snippet1.getPoint2().x) &&
-                (snippet1.getPoint1().x < snippet2.getPoint2().x && snippet2.getPoint2().x < snippet1.getPoint2().x);
+    public boolean isRemoveSnippet(Vector vector1, Vector vector2) {
+        return (vector1.getPoint1().x < vector2.getPoint1().x && vector2.getPoint1().x < vector1.getPoint2().x) &&
+                (vector1.getPoint1().x < vector2.getPoint2().x && vector2.getPoint2().x < vector1.getPoint2().x);
     }
 
-    public void merge(Vector snippet1, Vector snippet2) {
-        double maxPoint = snippet1.getPoint1().x;
-        double minPoint = snippet1.getPoint1().x;
+    public void merge(Vector vector1, Vector vector2) {
+        double maxPoint = vector1.getPoint1().x;
+        double minPoint = vector1.getPoint1().x;
 
-        double[] xPoints = {snippet1.getPoint1().x, snippet1.getPoint2().x, snippet2.getPoint1().x, snippet2.getPoint2().x};
+        double[] xPoints = {vector1.getPoint1().x, vector1.getPoint2().x, vector2.getPoint1().x, vector2.getPoint2().x};
 
         for (double xPoint : xPoints) {
             if (maxPoint < xPoint)
@@ -53,13 +53,13 @@ public class Shadow {
                 minPoint = xPoint;
         }
 
-        Point2D point1 = new Point2D(minPoint, snippet1.getPoint1().y);
-        Point2D point2 = new Point2D(maxPoint, snippet1.getPoint2().y);
+        Point2D point1 = new Point2D(minPoint, vector1.getPoint1().y);
+        Point2D point2 = new Point2D(maxPoint, vector1.getPoint2().y);
 
-        snippet1.setPoints(point1, point2);
+        vector1.setPoints(point1, point2);
     }
 
-    public void removeSnippet(Vector snippet, List<Vector> snippets) {
-        snippets.remove(snippet);
+    public void removeSnippet(Vector vector, List<Vector> vectors) {
+        vectors.remove(vector);
     }
 }
